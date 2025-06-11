f81fdf3967993de2350b05a82d95de3cef2337df
Daniel Ogunleye <danielayodeji97@gmail.com>
2025-06-11 17:16:20 +0100

    added project color and theming and font

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.danzucker.notemark"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.danzucker.notemark"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
