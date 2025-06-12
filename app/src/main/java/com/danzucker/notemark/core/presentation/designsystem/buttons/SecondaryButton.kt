15478241bee4b5024a6639496b83c7accda33a9a
Daniel Ogunleye <danielayodeji97@gmail.com>
2025-06-12 16:01:39 +0100

    design some reusable components

---
+++ app/src/main/java/com/danzucker/notemark/core/presentation/designsystem/buttons/SecondaryButton.kt
@@ -0,0 +1,100 @@
+package com.danzucker.notemark.core.presentation.designsystem.buttons
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.danzucker.notemark.core.presentation.designsystem.theme.NoteMarkTheme
+import com.danzucker.notemark.core.presentation.designsystem.values.Dimens.cornerRadiusSmallMedium12
+import com.danzucker.notemark.core.presentation.designsystem.values.Dimens.paddingLarge24
+import com.danzucker.notemark.core.presentation.designsystem.values.Dimens.paddingMedium16
+import com.danzucker.notemark.core.presentation.designsystem.values.Dimens.paddingSmallMedium12
+
+@Composable
+fun SecondaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false,
+    enabled: Boolean = true,
+) {
+    OutlinedButton(
+        onClick = onClick,
+        enabled = enabled && !isLoading,
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(size = cornerRadiusSmallMedium12),
+        border = BorderStroke(
+            width = 1.dp,
+            color = if (enabled) {
+                MaterialTheme.colorScheme.primary
+            } else {
+                Color.Transparent
+            }
+        ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = if (enabled) {
+                MaterialTheme.colorScheme.primary
+            } else {
+                MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
+            },
+            disabledContainerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+        ),
+        contentPadding = PaddingValues(
+            horizontal = paddingLarge24,
+            vertical = paddingSmallMedium12
+        )
+    ) {
+
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(15.dp),
+                strokeWidth = 1.5.dp,
+                color = if (enabled) {
+                    MaterialTheme.colorScheme.onPrimary
+                } else {
+                    MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
+                }
+            )
+        } else {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyMedium,
+                color = if (enabled) {
+                    MaterialTheme.colorScheme.primary
+                } else {
+                    MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f)
+                }
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun SecondaryButtonPreview() {
+    NoteMarkTheme {
+        SecondaryButton(
+            text = "Log In",
+            onClick = {},
+            enabled = true,
+            isLoading = false
+        )
+    }
+}
---
+++ app/src/main/java/com/danzucker/notemark/core/presentation/designsystem/values/Dimens.kt
@@ -0,0 +1,44 @@
+package com.danzucker.notemark.core.presentation.designsystem.values
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    // Padding
+    val paddingExtraSmall4 = 4.dp
+    val paddingSmall8 = 8.dp
+    val paddingSmallMedium12 = 12.dp
+    val paddingMedium16 = 16.dp
+    val paddingMediumLarge = 20.dp
+    val paddingLarge24 = 24.dp
+    val paddingExtraLarge32 = 32.dp
+
+    // Size
+    val sizeExtraSmall4 = 4.dp
+    val sizeSmall8 = 8.dp
+    val sizeSmallMedium12 = 12.dp
+    val sizeMedium16 = 16.dp
+    val sizeMediumLarge20 = 20.dp
+
+    // Corner radius
+    val cornerRadiusExtraSmall4 = 4.dp
+    val cornerRadiusSmall8 = 8.dp
+    val cornerRadiusSmallMedium12 = 12.dp
+    val cornerRadiusLarge16 = 16.dp
+    val cornerRadiusMediumLarge20 = 20.dp
+    val cornerRadiusExtraLarge24 = 24.dp
+
+    // Sizes
+    val iconSizeSmall = 24.dp
+    val iconSizeMedium = 32.dp
+    val iconSizeLarge = 48.dp
+
+    // Elevation
+    val elevationSmall = 2.dp
+    val elevationMedium = 4.dp
+    val elevationLarge = 8.dp
+
+    // Stroke widths
+    val strokeWidthSmall = 1.dp
+    val strokeWidthMedium12 = 2.dp
+}
